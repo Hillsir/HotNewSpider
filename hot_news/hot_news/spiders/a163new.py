479383d1b9897259a955c06e597f475c7f2deea2
LaoSi <hill@3io.cc>
2020-03-16 14:10:02 +0800

    网易爬虫

--- hot_news/hot_news/spiders/a163new.py
+++ hot_news/hot_news/spiders/a163new.py
@@ -11,20 +11,54 @@ class A163newSpider(scrapy.Spider):
 
     def parse(self, response):
         for i in response.xpath('//div[@class="tabBox"]//div[@class="tabContents"][2]//td//a'):
-
             url = i.xpath('@href').get()
-            title = i.xpath('text()').get()
-            yield Request(url, meta={"source_url": url, "title": title}, callback=self.get_details)
+            if not self.filter_url(url):
+                pass
+            else:
+                yield Request(url, meta={"source_url": url}, callback=self.get_details)
 
     def get_details(self, response):
 
         item = HotNewsItem()
-        item['content'] = [x.get()
-                           for x in response.xpath('//div[@class="post_text"]//p//text()')]
+
+        content = []
+        for x in response.xpath('//div[@class="post_text"]/p'):
+            x = x.xpath('text()').extract()
+            if x:
+                x = x[0].strip()
+                if x:
+                    content.append(x)
+        item['content'] = content
         classify = response.xpath(
             '//div[@class="post_crumb"]//a[2]/text()').get()
 
-        item['classification'] = classify if classify else "新闻"
         item['source_url'] = response.meta['source_url']
-        item['title'] = response.meta['title']
+        title, classification = self.hendle_title(
+            response.xpath('//title/text()').get())
+        item['title'] = title
+        item['classification'] = self.filter_classification(classify if classify else classification)
+
+        if not title and not content:
+            return
+
+
         yield item
+
+    def filter_url(self, url):
+        for i in ['play', 'auto','dy']:
+            if i in url:
+                return False
+        return True
+
+    def hendle_title(self, string: str):
+        if string and '_' in string:
+            string = string.split('_')
+            return string[0], string[1]
+        else:
+            return string, '新闻'
+
+    def filter_classification(self, classification:str):
+        for i in ['网易','频道','中心']:
+            if i in classification:
+                classification = classification.replace(i,'')
+        return classification
